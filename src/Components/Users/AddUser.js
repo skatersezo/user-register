cc8ba576a3a68c825b84e94c7efc5ab587df7c25
Ivan <ivansezoskate@gmail.com>
2021-06-07 00:35:20 +0900

    Builds Card class component and adds css styles

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
 
 function AddUser(props) {
 
@@ -7,14 +9,16 @@ function AddUser(props) {
     }
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
-            <label htmlFor="age">Age</label>
-            <input type="number" id="age" />
-            <button type="submit">Add User</button>
-        </form>
-    )
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" />
+                <label htmlFor="age">Age</label>
+                <input type="number" id="age" />
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
+    );
 }
 
 export default AddUser;
